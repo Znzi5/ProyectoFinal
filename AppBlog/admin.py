d7dd00e892759ebefcc99f149f70934a5d65ce8d
Znzi5 <agustin58345@gmail.com>
2023-12-24 07:48:47 -0300

    first commit

---
+++ AppBlog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from AppBlog.models import Product, Profile, Mensaje
+
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Profile)
+admin.site.register(Mensaje)
---
+++ AppBlog/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.forms import URLInput
+
+class BuscarProductoForm(forms.Form):
+    criterio_nombre = forms.CharField(max_length=100)
